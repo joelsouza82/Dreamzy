3c9e6336ed1fb1a70d0298caa2aff5fb8bd219e0
Joel de Almeida Souza <joelsouza82@gmail.com>
2023-02-25 14:49:23 -0300

    Update splash view

--- Dreamzy/ContentView.swift
+++ Dreamzy/ContentView.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Welcome to your lucid dream journal! Here you can record your dream experiences in a simple and fun way. Use texts, audio or images to capture the most important details of your dream and start exploring the fascinating world of lucid dreaming. We hope this app helps you improve your dreaming skills and that you have a lot of fun!")
-            .padding()
-            
+        VStack {
+            Text("Welcome to your lucid dream journal! Here you can record your dream experiences in a simple and fun way. Use texts, audio or images to capture the most important details of your dream and start exploring the fascinating world of lucid dreaming. We hope this app helps you improve your dreaming skills and that you have a lot of fun!")
+                .padding()
+                .foregroundColor(Color.blue)
+            Button(action: {
+                print("cliked")
+            }) {
+                HStack {
+                    Image(systemName: "sparkles")
+                        .font(.title)
+                    Text("Start")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(25)
+            }.padding(10)
+        }
+        
     }
 }
 
--- Dreamzy/View/SplashView/SplashView.swift
+++ Dreamzy/View/SplashView/SplashView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SplashView: View {
     @State var isActive : Bool = false
-    @State private var size = 0.8
+    @State private var size = 0.7
     @State private var opacity = 0.5
     
     var body: some View {
@@ -19,11 +19,19 @@ struct SplashView: View {
             VStack {
                 VStack {
                     Image("splash")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 128, height: 128)
+                        .cornerRadius(20)
+                    
+                    Text("Dreamzy")
+                        .font(Font.custom("Baskerville-Bold", size: 26))
+                        .foregroundColor(.black.opacity(0.80))
                 }
                 .scaleEffect(size)
                 .opacity(opacity)
                 .onAppear {
-                    withAnimation(.easeIn(duration: 1.1)) {
+                    withAnimation(.easeIn(duration: 1.2)) {
                         self.size = 0.9
                         self.opacity = 1.00
                     }
