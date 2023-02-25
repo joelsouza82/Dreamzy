8d2d34c852dceca4a8137fbbebcb05857033c05e
Joel de Almeida Souza <joelsouza82@gmail.com>
2023-02-25 09:47:23 -0300

    Create project

---
+++ Dreamzy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dreamzy
+//
+//  Created by Joel de Almeida Souza on 25/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dreamzy/DreamzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamzyApp.swift
+//  Dreamzy
+//
+//  Created by Joel de Almeida Souza on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DreamzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
